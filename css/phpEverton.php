9fb98fdd9b4cbc7dc2404172938daa88d44456c6
viny1992 <84174038+viny1992@users.noreply.github.com>
2021-05-16 17:49:39 +0200

    Update phpEverton.php

--- css/phpEverton.php
+++ css/phpEverton.php
@@ -1,7 +1,7 @@
 <?php
-    $nombre = &_post["Nomb"];
-    $apellido = &_post["Ape"];
+    $nombre = &_post("Nomb");
+    $apellido = &_post("Ape");
     echo 'Bienvenid@ .$nombre .$apellido.';
-    $correo = &_post["correo"]
+    $correo = &_post("correo");
     echo 'Te has susccrito a nuestra newsletter con el correo .$correo.';
 ?>
