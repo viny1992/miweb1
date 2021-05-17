d78159e7ca90d493940c58e2a09a30bec74cd42e
viny1992 <84174038+viny1992@users.noreply.github.com>
2021-05-17 12:18:30 +0200

    Add files via upload

--- css/phpEverton.php
+++ css/phpEverton.php
@@ -1,7 +1,10 @@
-<?php
-    $nombre = &_post["Nomb"];
-    $apellido = &_post["Ape"];
-    echo 'Bienvenid@ .$nombre .$apellido.';
-    $correo = &_post["correo"];
-    echo 'Te has susccrito a nuestra newsletter con el correo .$correo.';.
-?>
+<head>
+    <link rel="stylesheet" href="css/main.css" />
+</head>
+<body>
+    <?php
+        echo "Hola $_POST[Nomb] $_POST[Ape]";<
+        echo "Te has susccrito a nuestra newsletter con el correo   $_POST[correo]";
+    ?>  
+</body>
+
